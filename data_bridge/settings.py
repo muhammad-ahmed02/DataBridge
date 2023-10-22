e004c1037b56690e76d606eb47ef77d7fdf559f5
Ahmed <shiekhahmed135@gmail.com>
2023-10-23 04:44:41 +0500

    static files update

--- data_bridge/settings.py
+++ data_bridge/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(STATIC_ROOT, "/"),
+    os.path.join(STATIC_ROOT, "static"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
