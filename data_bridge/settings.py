60b4e1b938bf3e16f22b92e0fb52f1c8a4e0f4ae
Ahmed <shiekhahmed135@gmail.com>
2024-03-01 19:42:33 +0500

    Cloudinary storage added

--- data_bridge/settings.py
+++ data_bridge/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 from decouple import config
 
+import cloudinary
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
 
     'whitenoise.runserver_nostatic',
 
+    'cloudinary'
     'aws',
 ]
 
@@ -126,6 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_ROOT = str(BASE_DIR / "media")
 MEDIA_URL = "/media/"
@@ -139,3 +143,9 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/login/'
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUD_NAME"),
+    api_key=os.getenv('CLOUD_API_KEY'),
+    api_secret=os.getenv('CLOUD_API_SECRET')
+)
